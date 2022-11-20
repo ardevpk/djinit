afc13ff54a3b8593c9e9a6cf0cf104f9067da447
Abdul Rehman <adnan1470369258@gmail.com>
2022-11-20 21:27:15 +0500

    updated

--- build/lib/djinit/utilities/settings.py
+++ build/lib/djinit/utilities/settings.py
@@ -20,9 +20,9 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = "justcrawlera@gmail.com"
 EMAIL_HOST_PASSWORD = "jfmchfxgsebszbip"
 
-LOGIN_URL = 'signin'
+LOGIN_URL = 'accounts:signin'
 LOGIN_REDIRECT_URL = 'index'
-LOGOUT_REDIRECT_URL = 'signin'
+LOGOUT_REDIRECT_URL = 'accounts:signin'
 
 """
 
--- src/djinit/utilities/views.py
+++ src/djinit/utilities/views.py
@@ -3,9 +3,9 @@ import os
 
 def wrtingViewsFile(appPath:str,) -> bool:
     try:
-        viewContent = (f"from django.shortcuts import render\n")
+        viewContent = "from django.shortcuts import render\n\n\ndef index(request):\n\tcontext = {\n\t\t'': ''\n\t}\n\treturn render(request, 'index.html', context)"
         with open(os.path.join(appPath, 'views.py'), 'w') as vieww:
-            vieww.write(viewContent)
+            vieww.writelines(viewContent)
         return True
     except Exception as err:
         print('Views Error: ', err.__class__.__name__)
